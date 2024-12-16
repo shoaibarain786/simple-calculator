756082d6f2f52c25032d633ab09fb9a8755c0827
Shoaib:Shahid <shoaibarain789ali@gmail.com>
2024-12-16 11:20:50 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+const ans = await inquirer.prompt([{ 
+    type: "number",
+    name: "first_number",
+    message: "Enter you First Number: " 
+ },
+    {
+        type: "number", 
+        name: "second_number",
+        message: "Enter you Second Number: "
+     },
+    {
+        type: "list",
+        name: "operator",
+        message: "Please select operation type: ",
+        choices: ["Addition", "Subtracion", "Multiplication", "Division"]
+}
+])
+
+if (ans.operator === "Addition"){
+    console.log(ans.first_number + ans.second_number)
+} 
+else if (ans.operator === "Subtraction") {
+    console.log(ans.first_number - ans.second_number)
+}
+else if (ans.operator === "Multiplication") {
+    console.log(ans.first_number * ans.second_number)
+} 
+else{
+    console.log(ans.first_number / ans.second_number)
+}
